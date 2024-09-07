16757c550a834ce357e83a3153255da18e4ab755
Rohit <rohitgulati.scaler@gmail.com>
2024-09-08 03:05:23 +0530

    removing dummy Customer

--- src/main/java/com/happy/buddy/enums/Gender.java
+++ src/main/java/com/happy/buddy/enums/Gender.java
@@ -1,31 +1,12 @@
 package com.happy.buddy.enums;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
-import java.util.Map;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public enum Gender {
 
     MALE(0),
     FEMALE(1);
 
-
     Gender(int gender) {
     }
 
-//    private static final Map<String, Gender> FORMAT_MAP = Stream
-//            .of(Gender.values())
-//            .collect(Collectors.toMap(s -> s.gender, Function.identity()));
-//
-//
-//    @JsonCreator
-//    public static Gender fromString(String name) {
-//        return Optional
-//                .ofNullable(FORMAT_MAP.get(name))
-//                .orElseThrow(() -> new IllegalArgumentException(name));
-//    }
 }
--- src/main/java/com/happy/buddy/repositories/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.happy.buddy.repositories;
-
-import com.happy.buddy.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
