e8639177dbab05f8b703eb0ba50fcda8621331b2
Rohit <rohitgulati.scaler@gmail.com>
2024-12-13 02:51:58 +0530

    Show Facility Basics and Register Facility with Courts

--- src/main/java/com/happy/buddy/controllers/FacilityController.java
+++ src/main/java/com/happy/buddy/controllers/FacilityController.java
@@ -1,6 +1,7 @@
 package com.happy.buddy.controllers;
 
 
+import com.happy.buddy.entities.Activity;
 import com.happy.buddy.entities.Court;
 import com.happy.buddy.entities.CourtPk;
 import com.happy.buddy.entities.Facility;
@@ -14,6 +15,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin
@@ -28,27 +30,40 @@ public class FacilityController {
 
 
     @PostMapping
-    public Long addFacility(@RequestBody Facility facility) {
+    public Long addFacility(@RequestBody Facility facility) throws Exception {
         if (facility.getFacilityId() == null) {
             facility.setRegisteredOn(LocalDate.now());
         }
+        System.out.println(facility.getCourts().size() + " Courts Size ");
+        facility.getCourts().forEach( c -> System.out.println(c.getCourtFeatures()));
+
 
         Long facilityId = facilityRepository.save(facility).getFacilityId();
-        List<Court> courtList   = new ArrayList<>();
-        facility.getCourts().forEach(court -> {
-            try {
-                court.getId().setFacilityId(facilityId);
-                courtList.add(courtService.saveCourt(court));
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        });
+        facility.getCourts().forEach(court -> court.getId().setFacilityId(facilityId));
+        List<Court> courtList   = courtService.saveAllCourt(facility.getCourts());
+//        facility.getCourts().forEach(court -> {
+//            try {
+//                court.getId().setFacilityId(facilityId);
+//                courtList.add(courtService.saveCourt(court));
+//            } catch (Exception e) {
+//                throw new RuntimeException(e);
+//            }
+//        });
+        courtList.forEach(court -> System.out.println(court.getCourtFeatures()));
             return facilityId;
     }
 
     @GetMapping("{id}")
     public Facility getFacility(@PathVariable Long id) {
         return facilityRepository.findById(id).orElseThrow();
+    }
+
+
+    @GetMapping("/all/{city}")
+    public List<Facility> getFacilities(@PathVariable String city) {
+        return facilityRepository
+                   .findFacilitiesByisActiveAndFacilityAddress_City(true,city);
+
     }
 
 }
